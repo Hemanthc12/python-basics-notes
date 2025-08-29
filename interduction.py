23e63a2da5fd2d5c958f485d2d08fa37b1302f9e
hemanth_c <hemanth_c@tezhire.com>
2025-08-29 22:44:55 +0530

    Added Python notes on operators and conditionals

---
+++ interduction.py
@@ -0,0 +1,22 @@
+# %%
+print("Hello")
+# by adding # %%, you can create a cell in VS Code
+# %%
+print("Hello","to","Python")
+
+# %%
+print("number",end=":" )
+print(1)
+print(2)
+# %%
+name = input("Enter your name: ")
+print("welcome", name)
+
+# %%
+x = input("Enter First Number: ")
+y = input("Enter Second Number: ")
+x = int(x)
+y = int(y)
+res = x + y
+print("Sum is", res)
+# %%
